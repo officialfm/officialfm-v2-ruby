3a20689cf6763006a6875054406e270dce6b4fbd
Dimiter Petrov <crackofdusk@gmail.com>
2012-07-31 14:07:09 +0200

    Test helper method.

--- test/client_test.rb
+++ test/client_test.rb
@@ -15,7 +15,7 @@ class ClientTest < Test::Unit::TestCase
       OfficialFM::Client.new.api_key.should == API_KEY
     end
 
-   
+
     should "be able to override the default configuration" do
       DEFAULT_API_KEY = "test key"
       CUSTOM_API_KEY = "other test key"
@@ -26,6 +26,36 @@ class ClientTest < Test::Unit::TestCase
 
       client = OfficialFM::Client.new(:api_key => CUSTOM_API_KEY)
       client.api_key.should == CUSTOM_API_KEY
+
+    end
+
+    context "#resource_url" do
+      setup do
+        @base_url = 'http://example.com'
+        @client = OfficialFM::Client.new
+      end
+
+      should "not change the base paths of complete URLs" do
+        @client.send(:resource_url, @base_url, 'something', 'child').start_with?(@base_url).should be(true)
+      end
+
+      should "build resource url from a base url" do
+        @client.send(:resource_url, @base_url, 'something', 'child').should == 'http://example.com/child'
+      end
+
+      should "build resource url from an id" do
+        @client.send(:resource_url, "42", "projects", "tracks").should == '/projects/42/tracks'
+      end
+    end
+
+    context "#extend_resource" do
+      setup do
+        @client = OfficialFM::Client.new
+      end
+
+      should "add module methods to a client response object" do
+        #TODO
+      end
 
     end
 
